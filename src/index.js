f230c8bb5a4f25d4f4f09729a968b9969a3573d7
Tom Swindell <tom@zippie.org>
2018-08-02 13:01:50 +0100

    Implemented ITP 2.0 Support.
    Signed-off-by: Tom Swindell <tom@zippie.org>

--- src/index.js
+++ src/index.js
@@ -1,35 +1,73 @@
+/*
+ * Copyright (c) 2018 Zippie Ltd.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+*/
 var vault = null
+
 var vaultOpts = null
 var vaultReady = null
 var vaultNotReady = null
+
 var _counter = 0
 var receivers = {}
 
-function vaultHandleMessage(event) {
-  if (event.source == vault) {
-    if ('ready' in event.data) {
-      exports.message({ 'init' : vaultOpts }).then((result) => {
-        vaultReady(result)
-      }, (reject) => {
-        vaultNotReady(reject)
-      })
-      return;
-    } else {
-      if (event.data.callback && receivers[event.data.callback]) {
-        let receiver = receivers[event.data.callback]
-        delete receivers[event.data.callback]
-        if ('error' in event.data) {
-          if (event.data.error === 'launch' || event.data.error === 'signin') {
-            return launch(event.data.launch)
-          } else {
-            receiver[1](event.data)
-          }
-        } else {
-          receiver[0](event.data)
+/**
+ * Internal function, invoked when a message is received from vault.
+ */
+function onIncomingMessage(event) {
+  if (event.source !== vault) return;
+
+  console.log('API: Received:', event.data)
+
+  // Vault sent ready message, 
+  if ('login' in event.data) {
+    document.body.appendChild(exports.createButton())
+
+  } else if ('ready' in event.data) {
+    exports.message({ 'init' : vaultOpts })
+      .then(vaultReady, vaultNotReady)
+
+  } else {
+    if (event.data.callback && receivers[event.data.callback]) {
+      let receiver = receivers[event.data.callback]
+      delete receivers[event.data.callback]
+
+      if ('error' in event.data) {
+        if (event.data.error === 'launch' || event.data.error === 'signin') {
+          return launch(event.data.launch)
         }
+
+        return receiver[1](event.data)
       }
+
+      return receiver[0](event.data)
     }
-  }  
+  }
+}
+
+/**
+ * Internal function, invoked when vault asks us to redirect for signin/signup.
+ */
+function launch(vaultURI) {
+  console.log('redirecting to ' + vaultURI + '#?launch=' + window.location.href)
+  window.location = vaultURI + '#?launch=' + window.location.href
 }
 
 /**
@@ -43,29 +81,10 @@ exports.message = function (message) {
 
     receivers[id] = [resolve, reject]
     message.callback = id
+
     // XXX this should be to our known origin for when it claims it's ready
     vault.postMessage(message, '*')
   })
-}
-
-function launch(vaultURI) {
-  let uri = window.location.href.split('?')[0]
-  let hash = window.location.hash
-  let params = {}
-
-  // Process URI fragment part for vault params
-  if (hash.indexOf('?') !== -1) {
-    let p = hash.split('?')[1].split(';')
-
-    for (var i = 0; i < p.length; i++) {
-      var kv = p[i].split('=')
-      params[kv[0]] = kv[1]
-    }
-  }
-
-  // sort out deep linking
-  console.log('redirecting to ' + vaultURI + '#?launch=' + uri)
-  window.location = vaultURI + '#?launch=' + uri
 }
 
 /** 
@@ -92,17 +111,6 @@ exports.init = function (opts) {
     window.location.hash = hash.slice(0, hash.indexOf('?'))
   }
 
-  // Handle being loaded by vault inside iframe (legacy)
-  if (params['iframe']) {
-    opts.cookie = params['iframe']
-    opts.iframe = true
-  }
-
-  // Handle being launched by vault
-  if (params['zippie-vault']) {
-    opts.vaultURL = params['zippie-vault']
-  }
-
   // If no vault URI provided, auto-detect from domain check.
   if (!('vaultURL' in opts)) {
     opts.vaultURL = 'https://vault.zippie.org'
@@ -112,29 +120,65 @@ exports.init = function (opts) {
     }
   }
 
+  // XXX: Implement per-dapp vault access token and cookie, then dapps can
+  // cache in local storage their access token making this redirect only
+  // required on first run.
+
+  // If we have no zippie-vault URI, we have no access token, so launch vault.
+  if (params['zippie-vault'] === undefined) {
+    return launch(opts.vaultURL)
+  }
+
+  opts.vaultURL = params['zippie-vault']
+
+  // Create invisible, nested vault iframe.
   return new Promise(
     function (resolve, reject) {
       vaultOpts = opts
       vaultReady = resolve
       vaultNotReady = reject
 
-      window.addEventListener('message', vaultHandleMessage)
-      if (opts.iframe) {
-        vault = window.parent
+      window.addEventListener('message', onIncomingMessage)
 
-        console.log("Launched in an iframe, sending init message.")
-        return message({ 'init' : opts })
-      }
-      else {
-        var iframe = document.createElement('iframe')
-        iframe.style.display = 'none'
-        iframe.src = opts.vaultURL
-        document.body.appendChild(iframe)
+      var iframe = document.createElement('iframe')
+      iframe.style.display = 'none'
 
-        vault = iframe.contentWindow
+      iframe.sandbox += ' allow-storage-access-by-user-activation'
+      iframe.sandbox += ' allow-same-origin'
+      iframe.sandbox += ' allow-scripts'
 
-        console.log('Launched plainly, enclave built and waiting for ready signal.')
-        return Promise.resolve()
-      }
+      iframe.src = opts.vaultURL
+
+      document.body.appendChild(iframe)
+      vault = iframe.contentWindow
+
+      console.log('Launched plainly, enclave built and waiting for ready signal.')
+      return Promise.resolve()
     })
+}
+
+/**
+ * Create a Zippie Signin button.
+ * @return {Button} that when click signs the user in using Zippie
+ */
+exports.createButton = function () {
+  // XXX: Hand off button management to Dapp, so they can place it where they
+  // please. Also need to manage state visible, not visible, etc. properly.
+  var button = document.createElement('button')
+  button.id = 'zippie-btn'
+  button.style = 'position: absolute; right: 0; top: 0; border: 1px solid #000'
+  button.innerHTML = 'Zippie Signin'
+
+  button.onclick = function () {
+    console.log('API: Signing in with Zippie')
+    exports.message({'login': null})
+      .then(
+        result => {
+          button.style.display = 'none'
+        },
+        error => {
+        })
+  }
+
+  return button
 }
