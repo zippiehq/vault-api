d452d412de0b45246abc8ddabc7346b1f12e1601
Lewis Rockliffe <r0kk3rz@gmail.com>
2018-08-15 19:59:51 +0200

    Add more comprehensive tests

--- src/test.js
+++ src/test.js
@@ -1,26 +1,61 @@
+const vault = require('./index.js')
+const vaultSecp256k1 = require('./secp256k1.js')
+const shajs = require('sha.js')
 
+console.log('--- Zippie Vault Tests ---')
 
-var vault = require('./index.js')
-var vaultSecp256k1 = require('./secp256k1.js')
+console.log('--- Test #1: Init ---')
 
-opts = {}
+var opts = {vaultURL: 'https://vault.dev.zippie.org'}
 
-if (location.hash.startsWith('#zippie-vault=')) {
-   opts = { 'vaultURL' : location.hash.slice('#zippie-vault='.length) }
-}
-vault.init(opts).then((result) => {
-  console.log('got inited:')
-  console.log(result)
-  vaultSecp256k1.keyInfo(vault, 'm/0').then((result) => {
-    console.log(result)
-    vaultSecp256k1.sign(vault, 'm/0', '0123456789012345678901234567890123456789012345678901234567890101').then((result) => {
-      console.log(result)
-    })
-  })
-}, (error) => {
-  console.log('encountered error: ')
-  if (error.error === 'launch') {
-    vault.launch(error.launch)
+// Init Zippie vault
+vault.init(opts).then(
+  result => {
+    console.log('Init Result: ' + result);
+
+    console.log('--- Test #2: Sign ---')
+
+    // Sign data with derived private key for path 'm/0'
+     vaultSecp256k1
+      .sign(vault, 'm/0', shajs('sha256').update('data to sign goes here').digest())
+      .then(result => {
+        console.log("sign: " + result.signature)
+      });
+
+    console.log('--- Test #3: KeyInfo ---')
+    // Get derived public key for path 'm/0'
+
+    vaultSecp256k1.keyInfo(vault, 'm/1').then(result => {
+      console.log('keyInfo m/1: ' + result.pubkey);
+    });
+
+    vaultSecp256k1.keyInfo(vault, 'm/1/1').then(result => {
+      console.log('keyInfo m/1/1: ' + result.pubkey);
+    });
+
+    vaultSecp256k1.keyInfo(vault, 'm/0').then(result => {
+      console.log('keyInfo m/0: ' + result.pubkey);
+
+      console.log('--- Test #4: Encrypt ---')
+      // Encrypt a message
+      // NB: Usually you would use someone elses public key
+      vaultSecp256k1
+        .encrypt(vault, result.pubkey, Buffer.from("message to encrypt").toString('hex'))
+        .then(encResult => {
+          console.log("encrypt: " + encResult.result.ciphertext);
+
+          console.log('--- Test #5: Decrypt ---')
+          // Decrypt a message
+          vaultSecp256k1
+            .decrypt(vault, 'm/0', encResult.result)
+            .then(message => {
+              console.log("decrypt: " + Buffer.from(message.result, 'hex').toString());
+            });
+        });
+    });
+  },
+  error => {
+    console.log("encountered error: " + error);
   }
-  console.log(error)
-})
+);
+
