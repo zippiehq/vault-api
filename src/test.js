3e37d0c445c376f58038421136c0c6b7b357d539
Lewis Rockliffe <r0kk3rz@gmail.com>
2018-09-03 18:32:52 +0200

    use ES2015 import syntax

--- src/test.js
+++ src/test.js
@@ -1,6 +1,6 @@
-const vault = require('./index.js')
-const vaultSecp256k1 = require('./secp256k1.js')
-const shajs = require('sha.js')
+import * as vault from './index.js'
+import { keyInfo, sign, encrypt, decrypt } from './secp256k1.js'
+import * as shajs from 'sha.js'
 
 console.log('--- Zippie Vault Tests ---')
 
@@ -16,8 +16,7 @@ vault.init(opts).then(
     console.log('--- Test #2: Sign ---')
 
     // Sign data with derived private key for path 'm/0'
-     vaultSecp256k1
-      .sign(vault, 'm/0', shajs('sha256').update('data to sign goes here').digest())
+     sign(vault, 'm/0', shajs('sha256').update('data to sign goes here').digest())
       .then(result => {
         console.log("sign: " + result.signature)
       });
@@ -25,29 +24,27 @@ vault.init(opts).then(
     console.log('--- Test #3: KeyInfo ---')
     // Get derived public key for path 'm/0'
 
-    vaultSecp256k1.keyInfo(vault, 'm/1').then(result => {
+    keyInfo(vault, 'm/1').then(result => {
       console.log('keyInfo m/1: ' + result.pubkey);
     });
 
-    vaultSecp256k1.keyInfo(vault, 'm/1/1').then(result => {
+    keyInfo(vault, 'm/1/1').then(result => {
       console.log('keyInfo m/1/1: ' + result.pubkey);
     });
 
-    vaultSecp256k1.keyInfo(vault, 'm/0').then(result => {
+    keyInfo(vault, 'm/0').then(result => {
       console.log('keyInfo m/0: ' + result.pubkey);
 
       console.log('--- Test #4: Encrypt ---')
       // Encrypt a message
       // NB: Usually you would use someone elses public key
-      vaultSecp256k1
-        .encrypt(vault, result.pubkey, Buffer.from("message to encrypt").toString('hex'))
+      encrypt(vault, result.pubkey, Buffer.from("message to encrypt").toString('hex'))
         .then(encResult => {
           console.log("encrypt: " + encResult.result.ciphertext);
 
           console.log('--- Test #5: Decrypt ---')
           // Decrypt a message
-          vaultSecp256k1
-            .decrypt(vault, 'm/0', encResult.result)
+          decrypt(vault, 'm/0', encResult.result)
             .then(message => {
               console.log("decrypt: " + Buffer.from(message.result, 'hex').toString());
             });
