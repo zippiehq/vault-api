53931373a01759215097c768890c8c484ab65b80
Lewis Rockliffe <r0kk3rz@gmail.com>
2018-10-13 12:36:15 +0100

    add button graphics, make tests print to browser window

--- src/test.js
+++ src/test.js
@@ -1,58 +1,70 @@
+import { div, h1, span, br } from 'callbag-html'
 import * as vault from './index.js'
 import { keyInfo, sign, encrypt, decrypt } from './secp256k1.js'
 import * as shajs from 'sha.js'
 
-console.log('--- Zippie Vault Tests ---')
+function testLog(message)
+{
+  console.log(message)
+  document.body.appendChild(
+    div([span(message),
+    br()
+    ])
+  )
+}
 
-console.log('--- Test #1: Init ---')
+testLog('--- Zippie Vault Tests ---')
 
-var opts = {vaultURL: 'https://vault.dev.zippie.org'}
+testLog('--- Test #1: Init ---')
+
+var opts = {vaultURL: 'https://localhost:8443'}
 
 // Init Zippie vault
 vault.init(opts).then(
   result => {
-    console.log('Init Result: ' + result);
-
-    console.log('--- Test #2: Sign ---')
+    testLog('Init Result: ');
+    testLog(result)
+    
+    testLog('--- Test #2: Sign ---')
 
     // Sign data with derived private key for path 'm/0'
      sign(vault, 'm/0', shajs('sha256').update('data to sign goes here').digest())
       .then(result => {
-        console.log("sign: " + result.signature)
+        testLog("sign: " + result.signature)
       });
 
-    console.log('--- Test #3: KeyInfo ---')
+    testLog('--- Test #3: KeyInfo ---')
     // Get derived public key for path 'm/0'
 
     keyInfo(vault, 'm/1').then(result => {
-      console.log('keyInfo m/1: ' + result.pubkey);
+      testLog('keyInfo m/1: ' + result.pubkey);
     });
 
     keyInfo(vault, 'm/1/1').then(result => {
-      console.log('keyInfo m/1/1: ' + result.pubkey);
+      testLog('keyInfo m/1/1: ' + result.pubkey);
     });
 
     keyInfo(vault, 'm/0').then(result => {
-      console.log('keyInfo m/0: ' + result.pubkey);
+      testLog('keyInfo m/0: ' + result.pubkey);
 
-      console.log('--- Test #4: Encrypt ---')
+      testLog('--- Test #4: Encrypt ---')
       // Encrypt a message
       // NB: Usually you would use someone elses public key
       encrypt(vault, result.pubkey, Buffer.from("message to encrypt").toString('hex'))
         .then(encResult => {
-          console.log("encrypt: " + encResult.result.ciphertext);
+          testLog("encrypt: " + encResult.result.ciphertext);
 
-          console.log('--- Test #5: Decrypt ---')
+          testLog('--- Test #5: Decrypt ---')
           // Decrypt a message
           decrypt(vault, 'm/0', encResult.result)
             .then(message => {
-              console.log("decrypt: " + Buffer.from(message.result, 'hex').toString());
+              testLog("decrypt: " + Buffer.from(message.result, 'hex').toString());
             });
         });
     });
   },
   error => {
-    console.log("encountered error: " + error);
+    testLog("encountered error: " + error);
   }
 );
 
